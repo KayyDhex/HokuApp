e959a34d8f23dde02f412e297351eee76c980539
KayyDhex <37078492+KayyDhex@users.noreply.github.com>
2021-10-26 02:54:19 -0500

    add fiat library and first version of home

--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -35,7 +35,11 @@ if(AFAppX.registerApp) {
 
 function success() {
 require('../../app');
+require('../../components/fiat/f-input/f-input?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../pages/login/login?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/home/home?hash=f7bb9e069234027edc5bf8aa93ffd5640f0d1bbb');
+require('../../pages/info/info?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/profile/profile?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
 }
--- pages/login/login.js
+++ pages/login/login.js
@@ -3,5 +3,10 @@ const app = getApp();
 Page({
   signUp(){
     console.log("Hola mundo");
+  },
+  logIn(){
+    my.navigateTo({
+      url: '../home/home'
+    });
   }
   });
