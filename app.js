f424e173f80343c3778b046d79bf40b870854724
KayyDhex <37078492+KayyDhex@users.noreply.github.com>
2021-10-25 23:14:09 -0500

    add login first part
    Without Js logic

--- app.js
+++ app.js
@@ -6,4 +6,31 @@ App({
   onShow(options) {
     // Reopened by scheme from the background
   },
+
+  userInfo: null,
+  getUserInfo() {
+    return new Promise((resolve, reject) => {
+      if (this.userInfo) resolve(this.userInfo);
+
+      my.getAuthCode({
+        scopes: ['auth_user'],
+        success: authcode => {
+          console.info(authcode);
+
+          my.getAuthUserInfo({
+            success: res => {
+              this.userInfo = res;
+              resolve(this.userInfo);
+            },
+            fail: () => {
+              reject({});
+            },
+          });
+        },
+        fail: () => {
+          reject({});
+        },
+      });
+    });
+  },
 });
